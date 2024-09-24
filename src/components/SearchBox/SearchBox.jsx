bc311298dbe1916d02ba412142102bd3c52ab1c5
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-09-24 23:02:04 +0300

    correct

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,6 +1,3 @@
-// import { selectContacts } from "../../redux/contactsSlice";
-// import { useSelector } from "react-redux";
-// import { addContact } from "../../redux/contactsSlice";
 import { changeFilter } from "../../redux/filtersSlice";
 import css from "./SearchBox.module.css";
 
@@ -9,14 +6,11 @@ import { useDispatch} from "react-redux";
 
 export default function SearchBox() {
   const dispatch = useDispatch();
-  // const selectNameFilter = useSelector((state) => state.filters.name);
   
   const handleChange = (evn) => {
     // console.log("evn", evn.target.value);
     
-    
     //  dispatch введеного значання
-    
     // console.log("selectNameFilter", selectNameFilter);
     return dispatch(changeFilter(evn.target.value));
   };
